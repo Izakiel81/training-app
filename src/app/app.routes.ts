499cf94bd554c12db6ff2f22c394082d956d52cf
Maksym Habinskyi <mgabin348@gmail.com>
2025-06-02 01:26:54 +0200

    styling

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,5 +5,9 @@ export const routes: Routes = [
         path: '',
         pathMatch: 'full',
         loadComponent: () => import('./home/home.component').then(m => m.HomeComponent)
+    },
+    {
+        path: 'todos',
+        loadComponent: () => import('./todos/todos.component').then(m => m.TodosComponent)
     }
 ];
